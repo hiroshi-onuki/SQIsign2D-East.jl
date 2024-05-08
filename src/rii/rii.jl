1988ef0b5080be0f4ed388fbea4e0b76778629b4
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2024-05-08 11:28:04 +0900

    rii with images

--- src/rii/rii.jl
+++ src/rii/rii.jl
@@ -11,6 +11,7 @@ end
 function RandIsogImages(d::BigInt, E0_data::E0Data)
     deg_dim2 = BigInt(1) << ExponentFull
     a24_0 = E0_data.a24_0
+    xP0, xQ0, xPQ0 = E0_data.xP2e, E0_data.xQ2e, E0_data.xPQ2e
 
     alpha, _ = FullRepresentInteger(d*(deg_dim2 - d))
 
@@ -19,17 +20,24 @@ function RandIsogImages(d::BigInt, E0_data::E0Data)
         # the first (2,2)-isogeny is E0^2 -> E0^2 represented by the matrix [1 1; 1 -1]
         xP1, xQ1, xPQ1 = action_on_torsion_basis(alpha + d, E0_data)
         xP2, xQ2, xPQ2 = action_on_torsion_basis(alpha - d, E0_data)
+        xR1, xS1, xRS1 = xP0, xQ0, xPQ0
+        xR2, xS2, xRS2 = xP0, xQ0, xPQ0
         n = norm(alpha + d)
     elseif (alpha + deg_dim2*Quaternion_i) % 2 == Quaternion_i
         # the first (2,2)-isogeny is E0^2 -> E0^2 represented by the matrix [i 1; 1 i]
         xP1, xQ1, xPQ1 = action_on_torsion_basis(Quaternion_i * alpha - d, E0_data)
         xP2, xQ2, xPQ2 = action_on_torsion_basis(alpha - d * Quaternion_i, E0_data)
+        xR1, xS1, xRS1 = -xP0, -xQ0, -xPQ0
+        xR2, xS2, xRS2 = xP0, xQ0, xPQ0
         n = norm(Quaternion_i * alpha - d)
     else
         xP1, xQ1, xPQ1 = action_on_torsion_basis(alpha, E0_data)
-        xP2 = ladder(deg_dim2 - d, E0_data.xP2e, a24_0)
-        xQ2 = ladder(deg_dim2 - d, E0_data.xQ2e, a24_0)
-        xPQ2 = ladder(deg_dim2 - d, E0_data.xPQ2e, a24_0)
+        xP2 = ladder(deg_dim2 - d, xP0, a24_0)
+        xQ2 = ladder(deg_dim2 - d, xQ0, a24_0)
+        xPQ2 = ladder(deg_dim2 - d, xPQ0, a24_0)
+        O0 = infinity_point(parent(a24_0.X))
+        xR1, xS1, xRS1 = xP0, xQ0, xPQ0
+        xR2, xS2, xRS2 = O0, O0, O0
     end
 
     e = 0 # e is the number of (2, 2)-isogenies computed without theta functions
@@ -47,29 +55,29 @@ function RandIsogImages(d::BigInt, E0_data::E0Data)
         if e > 1
             if is_infinity(xDBLe(xP1, a24_0, ExponentFull-2))
                 K = xDBLe(xQ1, a24_0, ExponentFull-e)
-                Kd = xDBLe(xPQ1, a24_0, ExponentFull-e)
             else
                 K = xDBLe(xP1, a24_0, ExponentFull-e)
-                if is_infinity(xDBLe(xQ1, a24_0, ExponentFull-2))
-                    Kd = xDBLe(xPQ1, a24_0, ExponentFull-e)
-                else
-                    Kd = xDBLe(xQ1, a24_0, ExponentFull-e)
-                end
             end
-            a24_d, _ = two_e_iso(a24_0, Kd, e-1, [xP1, xQ1, xPQ1])
+            a24_1, (xP1, xQ1, xPQ1, xR1, xS1, xRS1) = two_e_iso(a24_0, K, e-1, [xP1, xQ1, xPQ1, xR1, xS1, xRS1])
             if is_infinity(xDBLe(xP2, a24_0, ExponentFull-2))
                 K = xDBLe(xQ2, a24_0, ExponentFull-e)
             else
                 K = xDBLe(xP2, a24_0, ExponentFull-e)
             end
-            a24_2, (xP2, xQ2, xPQ2)= two_e_iso(a24_0, K, e-1, [xP2, xQ2, xPQ2])
+            a24_2, (xP2, xQ2, xPQ2, xR2, xS2, xRS2)= two_e_iso(a24_0, K, e-1, [xP2, xQ2, xPQ2, xR2, xS2, xRS2])
         end
     end
 
     P1P2 = CouplePoint(xP1, xP2)
     Q1Q2 = CouplePoint(xQ1, xQ2)
     PQ1PQ2 = CouplePoint(xPQ1, xPQ2)
+    R1R2 = CouplePoint(xR1, xR2)
+    S1S2 = CouplePoint(xS1, xS2)
+    RS1RS2 = CouplePoint(xRS1, xRS2)
 
     strategy = compute_strategy(ExponentFull - e - 2, 2, 1)
-    product_isogeny_sqrt(a24_1, a24_2, P1P2, Q1Q2, PQ1PQ2, CouplePoint{FqFieldElem}[], ExponentFull - e, strategy)
+    Es, images = product_isogeny_sqrt(a24_1, a24_2, P1P2, Q1Q2, PQ1PQ2, [R1R2, S1S2, RS1RS2], ExponentFull - e, strategy)
+
+    xP, xQ, xPQ = images[1], images[2], images[3]
+    
 end
